e31e8e94f95813f01e63b7ebd64b33ecde2e284b
abdulmomen0099 <abdulmomen0099@gmail.com>
2020-01-26 00:22:56 +0600

    singup Fragment

--- app/src/main/java/com/momen/myecomarcefairbase/Fragment/SingUpFragment.java
+++ app/src/main/java/com/momen/myecomarcefairbase/Fragment/SingUpFragment.java
@@ -1,6 +1,8 @@
 package com.momen.myecomarcefairbase.Fragment;
 
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,12 +10,26 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.momen.myecomarcefairbase.Main2Activity;
+import com.momen.myecomarcefairbase.MainActivity;
 import com.momen.myecomarcefairbase.R;
 
 /**
@@ -21,12 +37,25 @@ import com.momen.myecomarcefairbase.R;
  */
 public class SingUpFragment extends Fragment {
 
-
     public SingUpFragment() {
         // Required empty public constructor
     }
+
+
+    //firbase auth
+    FirebaseAuth firebaseAuth;
+
+
+
+
     private TextView alrealHaveAnAcount;
     private FrameLayout parentFramelayout;
+    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+.[a-z]+";
+    private EditText emailText, fullNameText, passwordText, confirmPasswordtext;
+    private Button signUpbtn;
+    private ImageButton closeBtn;
+    private ProgressBar progressBar;
+
 
 
     @Override
@@ -38,6 +67,18 @@ public class SingUpFragment extends Fragment {
        parentFramelayout=getActivity().findViewById(R.id.regester_FrameLayoutId);
 
 
+
+        emailText = v.findViewById(R.id.signUpEmailId);
+        fullNameText = v.findViewById(R.id.signUpNameId);
+        passwordText = v.findViewById(R.id.signUpPasswordId);
+        confirmPasswordtext = v.findViewById(R.id.signUpConfirmPassid);
+        signUpbtn = v.findViewById(R.id.signUpBtnId);
+        closeBtn = v.findViewById(R.id.signUpCloseBtnId);
+        progressBar = v.findViewById(R.id.signUpprogressBarId);
+
+
+
+        firebaseAuth=FirebaseAuth.getInstance();
 
        return v;
     }
@@ -54,7 +95,91 @@ public class SingUpFragment extends Fragment {
 
             }
         });
+
+
+        emailText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                // when use typing then onTextChanged is called
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        fullNameText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // when use typing then onTextChanged is called
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        passwordText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // when use typing then onTextChanged is called
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        confirmPasswordtext.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // when use typing then onTextChanged is called
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
+        signUpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                  checkEmailAndPassword();
+
+            }
+        });
+
     }
+
 
     private void serFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction=getActivity().getSupportFragmentManager().beginTransaction();
@@ -64,4 +189,79 @@ public class SingUpFragment extends Fragment {
         fragmentTransaction.commit();
 
     }
+
+
+    private void checkInputs() {
+
+        if(!TextUtils.isEmpty(emailText.getText())){
+            if(!TextUtils.isEmpty(fullNameText.getText())){
+               if(!TextUtils.isEmpty(passwordText.getText())&& passwordText.length()>=8){
+                   if(!TextUtils.isEmpty(confirmPasswordtext.getText())){
+                       signUpbtn.setEnabled(false);
+                       signUpbtn.setTextColor(Color.rgb( 255, 255, 255));
+                   }else {
+                       signUpbtn.setEnabled(false);
+                       signUpbtn.setTextColor(Color.argb(50, 255, 255, 255));
+                   }
+               }else {
+                   signUpbtn.setEnabled(false);
+                   signUpbtn.setTextColor(Color.argb(50, 255, 255, 255));
+               }
+            }else {
+                signUpbtn.setEnabled(false);
+                signUpbtn.setTextColor(Color.argb(50, 255, 255, 255));
+            }
+
+        }else {
+            signUpbtn.setEnabled(false);
+            signUpbtn.setTextColor(Color.argb(50, 255, 255, 255));
+        }
+    }
+
+    private void checkEmailAndPassword() {
+
+        if(emailText.getText().toString().matches(emailPattern)){
+            if(passwordText.getText().toString().equals(confirmPasswordtext.getText().toString())){
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                signUpbtn.setEnabled(false);
+                signUpbtn.setTextColor(Color.argb(50, 255, 255, 255));
+
+                 firebaseAuth.createUserWithEmailAndPassword(emailText.getText().toString(),passwordText.getText().toString())
+                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                             @Override
+                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                 if(task.isSuccessful()){
+                                     Intent i= new Intent(getActivity(), Main2Activity.class);
+                                     startActivity(i);
+                                     getActivity().finish();
+
+                                 }else {
+                                     progressBar.setVisibility(View.INVISIBLE);
+                                     signUpbtn.setEnabled(false);
+                                     signUpbtn.setTextColor(Color.rgb( 255, 255, 255));
+
+                                     String error=task.getException().getMessage();
+                                     Toast.makeText(getActivity(), error, Toast.LENGTH_SHORT).show();
+                                 }
+                             }
+                         });
+            }else {
+                confirmPasswordtext.setError("password does't matched");
+
+            }
+        }else {
+
+            emailText.setError("Invalid Email");
+
+        }
+
+
+
+    }
+
+
+
+
 }
